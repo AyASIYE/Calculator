ad89779b290e930352f61ef255cde6ed214c3b80
AyASIYE <43359002+AyASIYE@users.noreply.github.com>
2019-01-29 18:54:17 -0500

    Update Calculate.java

--- src/Calculate.java
+++ src/Calculate.java
@@ -6,4 +6,8 @@ public class Calculate {
     public static double subtract (double num1 ,double num2 ){
         return num1-num2;
     }
+    public static double multiplication(double num1 ,double num2){
+        return num1*num2;
+    
+    }
 }
