576596a9f3bf00baa0d12d7e9f3c1915e3688b7c
AyASIYE <43359002+AyASIYE@users.noreply.github.com>
2019-01-30 01:04:23 -0500

    add divide method

--- src/Calculate.java
+++ src/Calculate.java
@@ -3,11 +3,17 @@ public class Calculate {
     public static double add(double num1, double num2){
       return  num1+num2;
     }
+    
     public static double subtract (double num1 ,double num2 ){
         return num1-num2;
     }
+    
     public static double multiplication(double num1 ,double num2){
         return num1*num2;
+     }
     
+    public static double divide(double num1,double num2){
+        return num1/num2;
     }
+    
 }
